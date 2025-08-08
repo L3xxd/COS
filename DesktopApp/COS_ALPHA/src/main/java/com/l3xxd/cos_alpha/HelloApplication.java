c09a161bca44bb799d3995ba3309b279f94f84f9
Alejandro Balderas <alexbalrios@gmail.com>
2025-08-07 19:42:42 -0600

    Login Update
    Adding icons and login structure

--- DesktopApp/COS_ALPHA/src/main/java/com/l3xxd/cos_alpha/HelloApplication.java
+++ DesktopApp/COS_ALPHA/src/main/java/com/l3xxd/cos_alpha/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1440, 1024);
-        stage.setTitle("Login");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("COS ALPHA V 1.1");
         stage.setScene(scene);
         stage.show();
     }
